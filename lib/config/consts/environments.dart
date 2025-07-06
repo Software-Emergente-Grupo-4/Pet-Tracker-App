ee07cf13839a52edc90f6e1e0cf77b323ce4fc06
Diego Castro <diego_castro_soto@hotmail.com>
2025-07-06 02:02:41 -0500

    feat: add chatbot and prediction API URL

--- lib/config/consts/environments.dart
+++ lib/config/consts/environments.dart
@@ -6,4 +6,6 @@ class Environment {
   }
 
   static String apiUrl = dotenv.env['API_URL'] ?? 'API URL not found';
+  static String chatBotUrl = 'https://pet-tracker-chatbot.azurewebsites.net/api/pet_tracker_chabot';
+  static String predictionServiceUrl = 'https://pet-tracker-prediction-service.azurewebsites.net/api/predict-health-measures'; 
 }
